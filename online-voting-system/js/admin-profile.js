791c7ff63bfc6fda8bfbdb906a00d5ca0123cb74
Agbolade Isikalu <agboladeisikalu@gmail.com>
2019-10-17 23:16:07 +0100

    my fifth commit

--- online-voting-system/js/admin-profile.js
+++ online-voting-system/js/admin-profile.js
@@ -1,9 +1,21 @@
+
+
+
 function makeTimer() {
 
-	//		var endTime = new Date("29 April 2018 9:56:00 GMT+01:00");	
-		var endTime = new Date("17 october 2019 1:09:00 GMT+01:00");			
-			endTime = (Date.parse(endTime) / 1000);
+    $('#deadline-submit').click((e) =>{
+      e.preventDefault();
+      var endTime
+      var value = $('#deadline').val()
+      //console.log(value)
+      //setInterval(function() {  endTime = new Date(value)}, 1000);
+     // localStorage.setItem('value',JSON.stringify(value))
 
+     
+      endTime = new Date(value);
+      //console.log(endTime);			
+			endTime = (Date.parse(endTime) / 1000);
+      console.log(endTime);
 			var now = new Date();
 			now = (Date.parse(now) / 1000);
 
@@ -23,39 +35,47 @@ function makeTimer() {
 			$("#hours").html(hours + "<span>Hours</span>");
 			$("#minutes").html(minutes + "<span>Minutes</span>");
       $("#seconds").html(seconds + "<span>Seconds</span>");	
+    })
+   // console.log(value);
+    //setInterval(function() {endTime = new Date(value);}, 1000);
+	//		var endTime = new Date("29 April 2018 9:56:00 GMT+01:00");	
       
-      if(timeLeft < 0 ) { $('#timer').hide();
-      endTime = 0;
-      $.ajax({
-        url:`http://localhost:3000/candidates`,
-        type:'GET',
-      dataType:'json',
-      success:function(data){
-        console.log(data);
-        var index
-        var arr1 = []
-        var highest = 0
-        var candidate 
-        data.forEach((person)=> {
-          arr1.push(Number(person.votes))
-        })
-        highest = Math.max(...arr1)
-        index = data.findIndex(function(person){
-          return person.votes == highest
-        })
-        console.log(data[index].name)
-      candidate = data[index].name
-      $("#result").html(`<h1 class="display-4">The Winner is ${candidate}</h1>`);
-   // $("#result-div").show();
-  }})}
+      
+  //     if(timeLeft < 0 ) { $('#timer').hide();
+  //     endTime = 0;
+  //     $.ajax({
+  //       url:`http://localhost:3000/candidates`,
+  //       type:'GET',
+  //     dataType:'json',
+  //     success:function(data){
+  //       console.log(data);
+  //       var index
+  //       var arr1 = []
+  //       var highest = 0
+  //       var candidate 
+  //       data.forEach((person)=> {
+  //         arr1.push(Number(person.votes))
+  //       })
+  //       highest = Math.max(...arr1)
+  //       index = data.findIndex(function(person){
+  //         return person.votes == highest
+  //       })
+  //       console.log(data[index].name)
+  //     candidate = data[index].name
+  //     $("#result").html(`<h1 class="display-4">The Winner is ${candidate}</h1>`);
+  //  // $("#result-div").show();
+  // }})}
         
   }
   
-  $("#result-div").hide();
+ // $("#result-div").hide();
 
-  $("#timer").hide();
+  $("#timer").show();
 
-//setInterval(function() { makeTimer(); }, 1000);
+setInterval(function() { makeTimer(); }, 1000);
+
+
+
 
 
 $(document).ready(function(){
@@ -72,6 +92,30 @@ $(document).ready(function(){
       
     }
   })
+ 
+  // populate total candidates
+  $.ajax({
+    url:`http://localhost:3000/candidates/`,
+    type:'GET',
+    dataType:'json',
+      success:function(data){
+      var allCandidates = data.length
+      $('#t-candidates').append(` ${allCandidates} Candidates`)
+      
+    }
+  })
+
+  // populate total voters
+  $.ajax({
+    url:`http://localhost:3000/voters/`,
+    type:'GET',
+    dataType:'json',
+      success:function(data){
+      var allVoters = data.length
+      $('#t-voters').append(` ${allVoters} Voters`)
+      
+    }
+  })
 
 
 // populate table for candidate
@@ -85,7 +129,7 @@ $(document).ready(function(){
       candidate+=`<tr><td>${person.name}</td>
       <td>${person.age}</td>
       <td>${person.sex}</td>
-      <td><button class="btn btn-secondary mr-2" onclick=editCandidateButton(${person.id})>Edit</button><button class="btn btn-danger" onclick=deleteCandidateButton(${person.id})>Delete</button></td></tr>`
+      <td><button class="btn btn-secondary mr-2" data-toggle="modal" data-target="#edit-modal" onclick=editCandidateButton(${person.id})>Edit</button><button class="btn btn-danger" onclick=deleteCandidateButton(${person.id})>Delete</button></td></tr>`
     })
 
     $('#candidate-table').append(candidate)
@@ -109,9 +153,28 @@ success:function(data){
   $('#voter-table').append(voter)
   }})
 
+  // search candidate table
+
+  $('#search').keyup(function() {
+    search_table($(this).val()); })
+  
+    function search_table(value){
+    $("#candidate-search tr").each(function(){
+      var found = 'false';
+        $(this).each(function(){
+          if($(this).text().toLowerCase().indexOf(value.toLowerCase()) >= 0){found = 'true'}});
+          if(found == 'true') {$(this).show()} else {$(this).hide();}
+      
+      }
+    )
+
+}
 
 })
 
+// FUNCTIONS SECTION
+
+// function to delete candidate
 
   function deleteCandidateButton(id) {
     const options = {
@@ -124,6 +187,8 @@ success:function(data){
     })  
   }
 
+  //function to delete voter
+
   function deleteVoterButton(id) {
     const options = {
       url:`http://localhost:3000/voters/${id}`,
@@ -135,6 +200,8 @@ success:function(data){
     })  
   }
 
+  // function to edit candidate
+
   function editCandidateButton(id) {
     const options = {
       url:`http://localhost:3000/candidates/${id}`,
@@ -145,13 +212,23 @@ success:function(data){
     $.ajax(options).done((data) => {
       var name = data.name;
       var email = data.email;
-      $('#edit-name').val(name)
+      var password = data.password;
+      var age = data.age;
+      var sex = data.sex;
+      var vote = data.votes;
 
+      $('#edit-name').val(name)
 
       $('#edit-form').submit(function(e){
         e.preventDefault();
         var name = $('#edit-name').val();
-        const updatedData = {"name": name,"email": email}
+        const updatedData = {"name": name,
+        "email": email,
+        "password": password,
+        "age": age,
+        "sex": sex,
+        "votes": vote}
+
         const options = {
            url:`http://localhost:3000/candidates/${id}`,
             type:'PUT',
@@ -162,26 +239,35 @@ success:function(data){
        })
       })}
 
+      // function to edit voter
+
       function editVoterButton(id) {
         const options = {
-          url:`http://localhost:3000/candidates/${id}`,
+          url:`http://localhost:3000/voters/${id}`,
           type:'GET',
           dataType: 'json',
         };
     
-    
         $.ajax(options).done((data) => {
           var name = data.name;
           var email = data.email;
+          var password = data.password;
+          var age = data.age;
+          var sex = data.sex;
+
           $('#edit-name').val(name)
-    
     
           $('#edit-form').submit(function(e){
             e.preventDefault();
             var name = $('#edit-name').val();
-            const updatedData = {"name": name,"email": email}
+            const updatedData = {"name": name,
+            "email": email,
+            "password": password,
+            "age": age,
+            "sex": sex}
+
             const options = {
-               url:`http://localhost:3000/candidates/${id}`,
+               url:`http://localhost:3000/voters/${id}`,
                 type:'PUT',
                 dataType: 'json',
                 data: updatedData
@@ -190,17 +276,7 @@ success:function(data){
            })
           })}
        
-  //     // const updatedData = {
-  //     //   name: data.name,
-  //     //   email: data.email,
-  //     //   age: data.age,
-  //     //   sex: data.sex,
-  //     //   occupation: data.occupation,
-  //     //   party: data.party,
-  //     //   post: data.post,
-  //     //   achievement: data.achievement
-  //     // };
-
+ 
   
   
   
