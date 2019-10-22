72c5f9b4bf500c01fbbf3ebeca36bae3c996df06
Agbolade Isikalu <agboladeisikalu@gmail.com>
2019-10-22 16:18:29 +0100

    my seventh commit

--- online-voting-system/js/admin-profile.js
+++ online-voting-system/js/admin-profile.js
@@ -105,6 +105,44 @@ success:function(data){
       )
     }
 
+
+    // smooth scrolling
+    $('#main-nav a').click(function(e)
+    {
+      if(this.hash !== '')
+      {
+        e.preventDefault();
+        var hash = this.hash;
+        $('html,body').animate
+        ({
+          scrollTop: $(hash).offset().top
+        }, 900, function(){window.location.hash = hash;})
+      }
+    })
+
+
+    // scroll top
+      $(window).scroll(function (){
+        if($(this).scrollTop() > 50 )
+        {
+          $('#back-to-top').fadeIn()
+        } else 
+        {
+          $('#back-to-top').fadeOut()
+        } 
+      });
+      // scroll body to 0px on click
+      $('#back-to-top').click(function ()
+    {
+      $('body,html').animate(
+        {
+          scrollTop:0
+        }, 400)
+        return false;
+    })
+    
+    
+
   })
 
 
